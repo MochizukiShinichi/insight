963cfdb7c64b37c9c42994fc6b9bb11e5adc263d
NJ <jiangnan8910@gmail.com>
2018-10-26 15:54:08 -0700

    no message

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo 'Begin computing'
-python ./src/run.py --InputFile='./input/h1b_input.csv' 
+python3 ./src/run.py --InputFile='./input/h1b_input.csv' 
 echo 'End computing, find files in output!'
