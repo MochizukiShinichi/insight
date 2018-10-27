008324b59e97b891bc5e27f17c6ad06282224a0e
NJ <jiangnan8910@gmail.com>
2018-10-26 17:03:37 -0700

    final

--- src/DataClass.py
+++ src/DataClass.py
@@ -1,8 +1,6 @@
 # - *- coding: utf- 8 - *-
-
 import csv
 import re
-
 from os.path import abspath
 
 class Data:
@@ -11,12 +9,8 @@ class Data:
 
      parameters:    
      ----------
-     file_path: string, the path of .csv file that holds data
-     job_name_id：string, name of the occupation name column in csv file
-     status_id: string, name of the status(certified, withdrawn...) column in the csv file
-     state_id: string, name of the state(CA, NH...) column in the csv file
-     class_id: string, name of the visa class(H1B...) column in the csv file
-     
+     file_path: string, the path of .csv file that holds input data
+    
      methods:
         ----------
         statistics methods
@@ -26,7 +20,7 @@ class Data:
                 --rtype: None
 
         ----------
-        i/o methods
+        output methods
         ----------
             generate_output_csv(self):
                 generating output *.csv file to output folder
@@ -44,7 +38,6 @@ class Data:
         """ method to read csv file and generate top_job and top_state
             --rtype: None
         """
-    
         with open(abspath('.') + self.file_path, newline='', encoding='utf_8') as file:
             # find indices correponds to column names
             try:
@@ -64,17 +57,19 @@ class Data:
                     top_job[row[job_name_index]] = 1 + top_job.get(row[job_name_index], 0) 
                     top_state[row[state_index]] = 1 + top_state.get(row[state_index], 0)
 
-            # sort job and state by number
+            # sort job and state by number and name
             self.top_job = [[job, top_job[job]] for job in sorted(top_job, key=lambda key: (-top_job.get(key), key)) if job != '']
             self.top_state = [[state, top_state[state]] for state in sorted(top_state, key=lambda key: (-top_state.get(key), key)) if state != '']
         
     def generate_output(self):
-        """method to output top results to txt files in output folder"""
+        """method to output top results to txt files in output folder
+            -- rtype: None
+        """
         job_cnt = len(self.top_job)
         state_cnt = len(self.top_state)
 
         if job_cnt == 0:
-            print('zero job categories in the input')
+            raise Exception('zero job categories in the input')
         else:    
             with open(abspath('.') + "/output/top_10_occupations.txt", 'w', newline='') as file:
                 writer = csv.writer(file, delimiter=';')
@@ -85,7 +80,7 @@ class Data:
                     writer.writerow([entry[0], entry[1], "{:.1%}".format(entry[1]/job_sum)])
           
         if state_cnt == 0:
-            print('zero state categories in the input')         
+            raise Exception('zero state categories in the input')         
         else:
             with open(abspath('.') + "/output/top_10_states.txt", 'w', newline='') as file:
                 writer = csv.writer(file, delimiter=';')
@@ -94,5 +89,3 @@ class Data:
                 state_sum = sum([cnt[1] for cnt in self.top_state[:min(10, state_cnt)]])
                 for entry in self.top_state[:min(10, state_cnt)]:
                     writer.writerow([entry[0], entry[1], "{:.1%}".format(entry[1]/state_sum)])
-
-
--- src/run.py
+++ src/run.py
@@ -1,14 +1,14 @@
 import DataClass
-from os.path import abspath
 
+# argument parser for command-line input, currently specifying only input file name
+# import argparse
+# parser = argparse.ArgumentParser(description='specifiy input file names')
+# parser.add_argument('--InputFile', help='name of input data file', required=True)
+# args = parser.parse_args()
+# input_file_name = args.InputFile
 
-import argparse
-parser = argparse.ArgumentParser(description='specifiy input file names')
-parser.add_argument('--InputFile', help='name of input data file', required=True)
-args = parser.parse_args()
-
-input_file_name = args.InputFile
+input_file_name = '/input/H1B_FY_2016.csv'
+# run data class to generate output
 dt = DataClass.Data(input_file_name)
-
 dt.generate_tops()
 dt.generate_output()
